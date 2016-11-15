8dc9a9fca332bf3effc08acc88e0b44beeffb937
Fayyaz Azam <fayyaza@moj.io>
2016-11-15 13:14:44 -0800

    [NOT PROD RDY] Added auto-prefixer for both scss and inline styling. Organized a little bit of webpack. Appropriated changes in Body.js. Removed _mixins since we don't need it for now

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import path from 'path';
+import autoprefixer from 'autoprefixer';
 
 export default {
   entry: './src/App.js',
@@ -10,22 +11,38 @@ export default {
   },
   module: {
     loaders: [
-      // Used for compiling ES2015 JavaScript
+      // Used for compiling JavaScript
       { test: /\.js/, loader: 'babel', exclude: /(node_modules)/},
       // Used for Bootstrap Less Source Files
       { test: /\.less/, loader: 'style!css!less' },
       // Used for compiling scss files
       { test: /\.scss$/, loaders: ["style", "css", "sass"]},
-      // Used for Bootstrap Less Source Files
-      { test: /\.css/, loader: 'style!css' },
-      // Used for Bootstrap Glyphicon Fonts
-      { test: /\.(woff2|woff|ttf|svg|eot)$/, loader: 'file' },
-      // Used for images
-      { test: /\.(jpe?g|png|gif|svg|ico)$/i, loader: 'file'}
+      // Used for post-css loader / auto-prefixing
+      { test: /\.css/, loader: 'style-loader!css-loader!postcss-loader' },
+      // Used for Images, Bootstrap Glyphicon Fonts
+      { test: /\.(jpe?g|png|gif|svg|ico|woff2|woff|ttf|svg|eot)$/i, loader: 'file'},
+        // Json loader so I can prefix inline styling
+      { test: /\.json$/, loader: "json-loader"}
     ]
+  },
+  postcss: [
+      autoprefixer({
+        browsers: ['last 3 versions']
+      })
+  ],
+
+  node: {
+    fs: "empty"
   },
 
   plugins: [
+    //Uncomment when building for production
+    /**
+     new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),**/
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: false
     })
