d4df45bbc07f8895d0afc544a1f69ebcce417fa1
Fayyaz Azam <fayyazazam@gmail.com>
2017-07-31 23:45:20 -0700

    Forgot to add proper text sizing, organized styling a little bit

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Intro = () => {
   const link = 'https://github.com/fayyazazam/fayyazazam.github.io/tree/v1-react-site';
   return (
-    <p id="intro">
+    <p className="pull-left">
       Site being built, stay tuned. <br />
       Check out the V1 version <a href={link}>here</a>.
     </p>
