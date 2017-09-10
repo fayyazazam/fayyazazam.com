1b6bec04b15e15140f14e9639b4b8325bb507ed8
Fayyaz Azam <fayyazazam@gmail.com>
2017-09-09 23:01:12 -0700

    Semblance of timeline, got rid of all the other garbage I was trying to do

--- app/components/Work.js
+++ app/components/Work.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Timeline, TimelineEvent } from 'react-event-timeline';
 
 const Work = () => {
   return (
@@ -8,18 +7,68 @@ const Work = () => {
     // Create a linked list-esque timeline
     // <p>Work Tab</p>
     <div style={{ float: 'right', width: '50%', marginTop: '3vh' }}>
-      <Timeline>
-        <TimelineEvent
-          title="Rise People"
-          createdAt="2017.06"
-          iconColor="#FF3D73"
-        />
-        <TimelineEvent
-          title="Mojio"
-          createdAt="2016.05 - 2016.12"
-          iconColor="#FF3D73"
-        />
-      </Timeline>
+      <section className="timeline-container">
+        <div className="timeline-container-before" />
+        {/* Rise People */}
+        <div className="event">
+          <div className="event-type now">
+            <span className="material-icons" />
+          </div>
+          <div className="event-container">
+            <div className="event-container-before" />
+            <div>
+              <div className="company">Rise People, Vancouver</div>
+              <div>2017.06</div>
+              <div>Software Developer</div>
+            </div>
+          </div>
+        </div>
+
+        {/* Mojio */}
+        <div className="event">
+          <div className="event-type">
+            <span className="material-icons" />
+          </div>
+          <div className="event-container">
+            <div className="event-container-before" />
+            <div>
+              <div className="company">Mojio, Vancouver</div>
+              <div>2016.05 - 2016.12</div>
+              <div>Software Developer Co-op</div>
+            </div>
+          </div>
+        </div>
+
+        {/* ABB */}
+        <div className="event">
+          <div className="event-type">
+            <span className="material-icons" />
+          </div>
+          <div className="event-container">
+            <div className="event-container-before" />
+            <div>
+              <div className="company">ABB, Richmond</div>
+              <div>2015.05 - 2015.12</div>
+              <div>Software Developer Co-op</div>
+            </div>
+          </div>
+        </div>
+
+        {/* GR */}
+        <div className="event">
+          <div className="event-type">
+            <span className="material-icons" />
+          </div>
+          <div className="event-container">
+            <div className="event-container-before" />
+            <div>
+              <div className="company">Global Relay, Vancouver</div>
+              <div>2015.01 - 2015.04</div>
+              <div>QA Analyst Co-op</div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
